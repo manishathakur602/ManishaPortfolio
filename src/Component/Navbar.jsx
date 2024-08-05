cd924921c88c96dbb727a5489e1b7d030a2da5bf
Manish6906 <20-m-0742@rayatbahrauniversity.edu.in>
2024-08-05 15:14:11 +0530

    update navbar

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { MdOutlineDarkMode } from "react-icons/md";
+import { FaRegUserCircle } from "react-icons/fa";
 import { Link } from 'react-router-dom';
 // import Logo from './../assets/Images/Screenshot 2024-07-19 151646.png';
 
@@ -10,7 +10,7 @@ const Navbar = () => {
         <div className='flex justify-between items-center p-4 bg-sky-300 rounded-lg'>
         
             <i className='font-medium cursor-pointer text-[1.4rem] sm:text-[2rem]'>PORTFOLIO</i>
-            <p className='text-[2rem] cursor-pointer ac'><MdOutlineDarkMode /></p>
+            <p className='text-[2rem] cursor-pointer ac'><FaRegUserCircle /></p>
 
    </div>
       </nav>
