0fb7cd77328fb26e56a82865c82ca714f035d29b
Manish6906 <manish980573@gmail.com>
2026-04-04 16:12:41 +0530

    add

--- src/Component/Resume.jsx
+++ src/Component/Resume.jsx
@@ -1,196 +1,176 @@
-import React from 'react'
-import Aos from 'aos';
-import "aos/dist/aos.css"
+import React, { useEffect } from "react";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
-// icon import
+// icons
 import { PiStudent } from "react-icons/pi";
 import { FaLayerGroup } from "react-icons/fa6";
 import { FaNetworkWired } from "react-icons/fa";
 
-
-
-
-
-// img import
+// img
 import Line from "./../assets/Line5.png";
 
 function Resume() {
+
+  useEffect(() => {
+    Aos.init({ duration: 800 });
+  }, []);
+
   return (
-    <>
-    <div className="p-3 mt-10 sm:mt-0 livvic-lighta">
-           <div className='bg-gray-400 rounded-lg p-3 sm:p-10'>
-                  <div className=" ">
-                      <h1 className="font-semibold text-[1.5rem] text-center sm:text-left">
-                      Resume
-                      </h1>
-                      <img src={Line} className="text-[1px]" />
+    <div className="p-3 sm:p-5 mt-10 sm:mt-0 livvic-lighta">
+      <div className="bg-gray-400 rounded-lg p-3 sm:p-6 lg:p-10">
+
+        {/* Header */}
+        <div>
+          <h1 className="font-semibold text-[1.5rem] text-center sm:text-left">
+            Resume
+          </h1>
+          <img src={Line} className="mt-2 w-[120px] sm:w-[150px]" />
+        </div>
+
+        {/* MAIN SECTION */}
+        <div
+          className="mt-5 grid grid-cols-1 xl:grid-cols-3 gap-5"
+          data-aos="fade-in"
+        >
+
+          {/* EDUCATION */}
+          <div className="p-3 bg-white rounded-2xl shadow-md shadow-black">
+            <div className="flex items-center gap-2">
+              <PiStudent className="text-red-700 text-[1.2rem]" />
+              <h1 className="font-semibold">Education</h1>
+            </div>
+
+            <div className="mt-3 bg-red-200 rounded-xl p-4">
+              <div className="flex flex-col sm:flex-row sm:justify-between gap-3">
+                
+                <div className="border-b-2 border-gray-700 p-2">
+                  <h1 className="font-semibold text-gray-500">Degree</h1>
+                  <p className="text-[13px]">Doaba Group of college</p>
+                </div>
+
+                <div className="border-b-2 border-gray-700 p-2 text-left sm:text-center">
+                  <h1 className="font-semibold text-gray-500">Passing</h1>
+                  <p className="text-[13px]">2021-24</p>
+                </div>
+
+                <div className="border-b-2 border-gray-700 p-2 text-left sm:text-center">
+                  <h1 className="font-semibold text-gray-500">CGPA</h1>
+                  <p className="text-[13px]">8.17</p>
+                </div>
+
+              </div>
+            </div>
+          </div>
+
+          {/* EXPERIENCE */}
+          <div className="p-3 bg-white rounded-2xl shadow-md shadow-black">
+            <div className="flex items-center gap-2">
+              <FaLayerGroup className="text-red-700 text-[1.2rem]" />
+              <h1 className="font-semibold">Experience</h1>
+            </div>
+
+            <div className="mt-3 space-y-3">
+
+              {/* Item */}
+              <div className="bg-red-200 rounded-xl p-4">
+                <div className="flex flex-col sm:flex-row sm:justify-between gap-3">
+                  
+                  <div className="border-b-2 border-gray-700 p-2">
+                    <h1 className="font-semibold text-gray-500 text-[13px]">
+                      1. Company Name
+                    </h1>
+                    <p className="text-[13px]">Kreativan technologies</p>
                   </div>
-{/*  main section of both group */}
-           <div className='mt-5  xl:flex  xl:gap-2 ' data-aos="fade-in">
-                    {/* first section */}
-                    <div className='  p-3 bg-white rounded-2xl shadow-md shadow-black' >
-                            <div className='flex items-center gap-2'>
 
-                            <PiStudent className="text-red-700 text-[1.2rem]" />
-                                  <h1 className='font-semibold'>Education </h1>
-                            </div>
-                            <div className="mt-3 bg-red-200 rounded-xl p-4">
-                                  <div className=" sm:flex sm:gap-7 sm:items-center">
-                                  
-                                        <div className=' border-solid border-b-2 border-gray-700 rounded-md p-2'>
-                                        <h1 className="font-semibold text-gray-500">Degree</h1>
-                                        <p className='text-[13px]'>Doaba Group of college</p>
-                                        </div>
-                                        <div className='sm:flex sm:flex-col sm:items-center border-solid border-b-2 border-gray-700 rounded-md p-2'>
-                                        <h1 className="font-semibold text-gray-500">Passing</h1>
-                                        <p className='text-[13px]'>2021-24</p>
-                                        </div>
-                                        <div className='sm:sm:flex flex-col sm:items-center border-solid border-b-2 border-gray-700 rounded-md p-2'>
-                                        <h1 className="font-semibold text-gray-500">CGPA</h1>
-                                        <p className='text-[13px]'>8.17</p>
-                                        </div>
-                                  </div>
-                                  
-                                </div>
-                             </div>
+                  <div className="border-b-2 border-gray-700 p-2 text-left sm:text-center">
+                    <h1 className="font-semibold text-gray-500">From</h1>
+                    <p className="text-[13px]">15-Jan-24</p>
+                  </div>
 
-                             {/* second section */}
+                  <div className="border-b-2 border-gray-700 p-2 text-left sm:text-center">
+                    <h1 className="font-semibold text-gray-500">To</h1>
+                    <p className="text-[13px]">30-Jan-25</p>
+                  </div>
 
-                             <div className='sm:mt-5 xl:mt-0' >
-                             <div className=' p-3 bg-white rounded-2xl shadow-md  shadow-black mt-6 lg:mt-0 sm:p-9 lg:p-2 '>
-                            <div className='flex items-center gap-2 '>
+                </div>
+              </div>
 
-                            <FaLayerGroup className="text-red-700 text-[1.2rem]" />
-                                  <h1 className='font-semibold'>Experience </h1>
-                            </div>
-                            <div className="mt-3 bg-red-200 rounded-xl  p-4 text-center ">
-                                  <div className="flex gap-3 ">
-                                  
-                                        <div className=' border-solid border-b-2 border-gray-700 rounded-md p-2'>
-                                        <h1 className="font-semibold text-gray-500 text-[13px]">1. Company Name</h1>
-                                        <p className='text-[13px]'>Kreativan technologies</p>
-                                        </div>
-                                        <div className='sm:flex sm:flex-col sm:items-center border-solid border-b-2 border-gray-700 rounded-md p-2'>
-                                        <h1 className="font-semibold text-gray-500">From</h1>
-                                        <p className='text-[13px]'>15-Jan-24</p>
-                                        </div>
-                                        <div className='sm:sm:flex flex-col sm:items-center border-solid border-b-2 border-gray-700 rounded-md p-2'>
-                                        <h1 className="font-semibold text-gray-500">To</h1>
-                                        <p className='text-[13px]'>30-Jan-25</p>
-                                        </div>
-                                  </div>
-                                </div>
+              <div className="bg-red-200 rounded-xl p-4">
+                <div className="flex flex-col sm:flex-row sm:justify-between gap-3">
+                  
+                  <div className="border-b-2 border-gray-700 p-2">
+                    <h1 className="font-semibold text-gray-500 text-[13px]">
+                      2. Company Name
+                    </h1>
+                    <p className="text-[13px]">DigiSky Fusion</p>
+                  </div>
 
+                  <div className="border-b-2 border-gray-700 p-2 text-left sm:text-center">
+                    <h1 className="font-semibold text-gray-500">From</h1>
+                    <p className="text-[13px]">03-Feb-25</p>
+                  </div>
 
-                               <div className='sm:flex gap-3'>
-                                     <div className="mt-3 bg-red-200 rounded-xl  p-4 text-center ">
-                                  <div className="flex gap-3 ">
-                                  
-                                        <div className=' border-solid border-b-2 border-gray-700 rounded-md p-2'>
-                                        <h1 className="font-semibold text-gray-500 text-[13px]">2.Company Name</h1>
-                                        <p className='text-[13px]'>DigiSky Fusion</p>
-                                        </div>
-                                        <div className='sm:flex sm:flex-col sm:items-center border-solid border-b-2 border-gray-700 rounded-md p-2'>
-                                        <h1 className="font-semibold text-gray-500">From</h1>
-                                        <p className='text-[13px]'>03-Feb-25</p>
-                                        </div>
-                                        <div className='sm:sm:flex flex-col sm:items-center border-solid border-b-2 border-gray-700 rounded-md p-2'>
-                                        <h1 className="font-semibold text-gray-500">To</h1>
-                                        <p className='text-[13px]'>30-June-2025</p>
-                                        </div>
-                                  </div>
-                                </div>
-                                 <div className="mt-3 bg-red-200 rounded-xl  p-4 text-center ">
-                                  <div className="flex gap-3 ">
-                                  
-                                        <div className=' border-solid border-b-2 border-gray-700 rounded-md p-2'>
-                                        <h1 className="font-semibold text-gray-500 text-[13px]">3.Company Name</h1>
-                                        <p className='text-[13px]'>Metaponder Solution</p>
-                                        </div>
-                                        <div className='sm:flex sm:flex-col sm:items-center border-solid border-b-2 border-gray-700 rounded-md p-2'>
-                                        <h1 className="font-semibold text-gray-500">From</h1>
-                                        <p className='text-[13px]'>01-Sept-25</p>
-                                        </div>
-                                        <div className='sm:sm:flex flex-col sm:items-center border-solid border-b-2 border-gray-700 rounded-md p-2'>
-                                        <h1 className="font-semibold text-gray-500">To</h1>
-                                        <p className='text-[13px]'>Present</p>
-                                        </div>
-                                  </div>
-                                </div>
-                               </div>
+                  <div className="border-b-2 border-gray-700 p-2 text-left sm:text-center">
+                    <h1 className="font-semibold text-gray-500">To</h1>
+                    <p className="text-[13px]">30-June-2025</p>
+                  </div>
 
-                             </div>
+                </div>
+              </div>
 
+              <div className="bg-red-200 rounded-xl p-4">
+                <div className="flex flex-col sm:flex-row sm:justify-between gap-3">
+                  
+                  <div className="border-b-2 border-gray-700 p-2">
+                    <h1 className="font-semibold text-gray-500 text-[13px]">
+                      3. Company Name
+                    </h1>
+                    <p className="text-[13px]">Metaponder Solution</p>
+                  </div>
 
+                  <div className="border-b-2 border-gray-700 p-2 text-left sm:text-center">
+                    <h1 className="font-semibold text-gray-500">From</h1>
+                    <p className="text-[13px]">01-Sept-25</p>
+                  </div>
 
-                             
+                  <div className="border-b-2 border-gray-700 p-2 text-left sm:text-center">
+                    <h1 className="font-semibold text-gray-500">To</h1>
+                    <p className="text-[13px]">Present</p>
+                  </div>
 
-                             </div>
-
-
-
-
-                              <div className='mt-5 xl:mt-0' >
-                             <div className='  p-3 bg-white rounded-2xl shadow-md shadow-black mt-5  lg:mt-0 text-center  '>
-                            <div className='flex items-center gap-2 '>
-
-                            <FaNetworkWired className="text-red-700 text-[1.2rem]" />
-                                  <h1 className='font-semibold'>Work Skills </h1>
-                            </div>
-                            <div className="mt-3  bg-red-200 rounded-xl p-4 sm:p-6 text-[16px] text-gray-600">
-                                  <div className=" flex gap-5 flex-wrap items-center font-semibold">
-                                  <h1 className='underline'>HTML5</h1>
-                                  <h1 className='underline'>CSS3</h1>
-                                  <h1 className='underline'>TAILWIND CSS</h1>
-                                  <h1 className='underline'>BOOTSTRAP</h1>
-                                  <h1 className='underline'>JAVASCRIPT</h1>
-                                  <h1 className='underline'>REACT.JS</h1>
-                                  <h1 className='underline'>NEXT.JS (Basic)</h1>
-                                  {/* <h1 className='underline'>Node.js</h1> */}
-                                  {/* <h1 className='underline'>Express.js</h1> */}
-                                  {/* <h1 className='underline'>MongoDB</h1> */}
-                                  <h1 className='underline'>GIT</h1>
-                                  <h1 className='underline'>GITHUB/Gitlab</h1>
-                                  <h1 className='underline'>Postman</h1>
-                                  {/* <h1 className='underline'>Wordpress</h1> */}
-                                  {/* <h1 className='underline'>Figma</h1> */}
-                                  </div>
-                                  
-                                </div>
-                             </div>
-                              </div>
-
-
-
-
-                             </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+                </div>
+              </div>
 
             </div>
-     </div>
-    </>
-  )
+          </div>
+
+          {/* SKILLS */}
+          <div className="p-3 bg-white rounded-2xl shadow-md shadow-black">
+            <div className="flex items-center gap-2">
+              <FaNetworkWired className="text-red-700 text-[1.2rem]" />
+              <h1 className="font-semibold">Work Skills</h1>
+            </div>
+
+            <div className="mt-3 bg-red-200 rounded-xl p-4 sm:p-6 text-[16px] text-gray-600">
+              <div className="flex flex-wrap gap-3 font-semibold">
+                {[
+                  "HTML5","CSS3","TAILWIND CSS","BOOTSTRAP",
+                  "JAVASCRIPT","REACT.JS","NEXT.JS (Basic)",
+                  "GIT","GITHUB/Gitlab","Postman"
+                ].map((skill, i) => (
+                  <h1 key={i} className="underline text-sm sm:text-base">
+                    {skill}
+                  </h1>
+                ))}
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Resume
+export default Resume;
